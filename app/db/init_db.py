703ea524f6499311e702a7fe9ec10a9a57816e32
Diego Lizarraga <lizarragasanchezdiego@gmail.com>
2026-05-18 16:42:20 -0600

    nuevos cambios de server

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -3,6 +3,9 @@ from sqlmodel import SQLModel
 from app.db.session import engine
 from app.models.user import User
 from app.models.post import Post
+from app.models.comment import Comment
+from app.models.like import Like
+from app.models.image import Image
 
 
 async def init_db():
